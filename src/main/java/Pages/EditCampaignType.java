d8b5ca5c5222a2362bf2e8ffcd4beaa442158ab7
VietTuan <leviettuan717@gmail.com>
2025-11-18 14:56:44 +0700

    Add Edit Campaign Type

--- src/main/java/Pages/EditCampaignType.java
+++ src/main/java/Pages/EditCampaignType.java
@@ -1,4 +1,15 @@
 package Pages;
 
-public class EditCampaignType {
+import models.Campaign;
+import org.openqa.selenium.By;
+
+public class EditCampaignType extends BasePage {
+
+    private final By campaignTypeName = By.id("editformcampaigntype:ctn");
+
+
+    public void enterCampaignTypeName(Campaign campaign) {
+        clear(campaignTypeName);
+        type(campaignTypeName, campaign.getCampaignTypeName());
+    }
 }
