f71d8fb5e9de2b9e19744786afd1e1d2ac0bc8ab
VietTuan <leviettuan717@gmail.com>
2025-11-18 14:36:33 +0700

    Update Driver

--- src/main/java/Utils/Driver.java
+++ src/main/java/Utils/Driver.java
@@ -1,7 +1,26 @@
 package Utils;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Driver {
     public static WebDriver WEB_DRIVER;
+
+
+
+    public static final ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
+
+    public static void setDrivers(WebDriver driver) {
+        drivers.set(driver);
+    }
+
+    public static WebDriver getWebDriver() {
+        return drivers.get();
+    }
+
+    public static WebDriverWait getWebdriverWait() {
+        return new WebDriverWait(getWebDriver(), Duration.ofSeconds(10));
+    }
 }
