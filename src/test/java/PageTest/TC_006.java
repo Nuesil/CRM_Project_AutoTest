9f9adab5fefada5cd2e41020fb512dce92008b84
VietTuan <leviettuan717@gmail.com>
2025-11-24 20:39:06 +0700

    Update file

--- src/test/java/PageTest/TC_006.java
+++ src/test/java/PageTest/TC_006.java
@@ -3,35 +3,40 @@
 // (powered by FernFlower decompiler)
 //
 
-package CampaignTest;
+package PageTest;
 
 import TestBase.TestBase;
 import Pages.CreateCampaignPage;
 import Pages.HomePage;
 import Pages.LoginPage;
+import Utils.Config;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TC_06 extends TestBase {
-    private static final Logger log = LogManager.getLogger(TC_06.class);
+public class TC_006 extends TestBase {
+    private static final Logger log = LogManager.getLogger(TC_006.class);
     LoginPage loginPage = new LoginPage();
     HomePage homepage = new HomePage();
     CreateCampaignPage createCampaignPage = new CreateCampaignPage();
 
-    @Test
+    @Test(description = "Test 06: Verify “New Campaign” button navigates to Create Campaign page")
     public void Test06() {
-        log.info("Verify “New Campaign” button navigates to Create Campaign page");
+
         log.info("Step 1: Logging in with valid username and password");
-        loginPage.Login("vyvanviet@gmail.com", "abc123");
+        loginPage.Login(Config.VALID_USERNAME, Config.VALID_PASSWORD);
+
         log.info("Step 2: Click Campaign");
         homepage.clickCampaigns();
+
         log.info("Step 3: Click Show All Campaign");
         homepage.clickShowAllCampaign();
+
         log.info("Step 4: Open Create Campaign Page");
         homepage.openCreateCampaignPage();
+
         log.info("Step 5: Assert url matches");
-        Assert.assertTrue(this.createCampaignPage.getTitle().contains("Create Campaign"), "Create Campaign is not displayed");
+        Assert.assertTrue(createCampaignPage.getTitle().contains("Create Campaign"), "Create Campaign is not displayed");
     }
 }
--- src/test/java/PageTest/TC_008.java
+++ src/test/java/PageTest/TC_008.java
@@ -44,7 +44,7 @@ public class TC_008 extends TestBase {
         showAllCamPaignPage.filterCampaignName(campaign);
 
         log.info("Step 7: Assert Campaign name matches");
-        Assert.assertEquals(showAllCamPaignPage.getFirstCampaignName(), Campaign.getListCampaignData(campaign));  // todo: ko goi dc data
+        Assert.assertEquals(showAllCamPaignPage.getFirstCampaignName(), Campaign.getListCampaignData(campaign));
 
      }
 
