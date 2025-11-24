cc3279f34483256c10beb9d90121c48e29307970
VietTuan <leviettuan717@gmail.com>
2025-11-24 18:32:55 +0700

    Update file

--- src/main/java/Pages/CreateCampaignTypePage.java
+++ src/main/java/Pages/CreateCampaignTypePage.java
@@ -9,14 +9,11 @@ public class CreateCampaignTypePage extends BasePage {
     private final By saveLocator = By.cssSelector("input[type='submit']");
     private final By campaignTypeName = By.id("campaigntypeform:ctn");
 
-    public void save() {
-        click(saveLocator);
-    }
 
     public void enterCampaignTypeName(Campaign campaign) {
         type(campaignTypeName, campaign.getCampaignTypeName());
+        click(saveLocator);
     }
-
 
 
 }
