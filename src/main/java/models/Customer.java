3688f33beb5014aa2ba619ed00da0a062fde17ac
VietTuan <leviettuan717@gmail.com>
2025-11-24 18:33:58 +0700

    Update file

---
+++ src/main/java/models/Customer.java
@@ -0,0 +1,13 @@
+package models;
+
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+    private String Name;
+    private String Email;
+    private String Phone;
+    private String Address;
+}
