bef2004b224835d52b2fa07345a5be44528c7a1b
VietTuan <leviettuan717@gmail.com>
2025-11-24 18:31:54 +0700

    Update file

--- src/main/java/data/campaignData.java
+++ src/main/java/data/campaignData.java
@@ -3,7 +3,6 @@ package data;
 import Utils.FakerData;
 import models.Campaign;
 import java.time.LocalDate;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class campaignData {
 
@@ -15,12 +14,17 @@ public class campaignData {
         campaign.setStatus(FakerData.faker.options().option("Done", "Yet"));
         campaign.setStartDate(LocalDate.now());
         campaign.setEndDate(LocalDate.now().plusDays(7));
-        campaign.setExpectedRevenue(String.valueOf(FakerData.faker.number().numberBetween(1000,3000))); // kieu du lieu
-        campaign.setActualCost(String.valueOf(FakerData.faker.number().numberBetween(1000,3000)));
+        campaign.setExpectedRevenue((int) FakerData.faker.number().numberBetween(1000, 3000));
+        campaign.setBudgetedCost((int) FakerData.faker.number().numberBetween(1000, 5000));
+        campaign.setActualCost((int) FakerData.faker.number().numberBetween(1000, 3000));
         campaign.setDescription(FakerData.faker.lorem().sentence());
         campaign.setCampaignTypeName("discount" + FakerData.faker.number().numberBetween(1, 100) + "%");
         return campaign;
     }
+
+
+
+
 
 
 
